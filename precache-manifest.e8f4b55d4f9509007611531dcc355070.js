37e2a40351a2c01dd87c590cf24c7e6ba22cc64a
cariasp27 <paul.carias@yahoo.com>
2019-06-17 21:41:17 -0400

    Updates

---
+++ precache-manifest.e8f4b55d4f9509007611531dcc355070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702ecb02f295659ca78b5110cb16303e",
+    "url": "/ClicKlack/index.html"
+  },
+  {
+    "revision": "7bbe0d8edcb3a8321d1b",
+    "url": "/ClicKlack/static/css/main.a791f79c.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/ClicKlack/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "7bbe0d8edcb3a8321d1b",
+    "url": "/ClicKlack/static/js/main.0b781826.chunk.js"
+  },
+  {
+    "revision": "e75c1a1740655e4fe720",
+    "url": "/ClicKlack/static/js/runtime~main.20ec97bc.js"
+  },
+  {
+    "revision": "e59f32fa1bbc7a39de392575c2c00501",
+    "url": "/ClicKlack/static/media/blackorchid_@2X.e59f32fa.jpg"
+  }
+]);
